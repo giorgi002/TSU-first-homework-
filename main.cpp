07e3891350d849835e2b5b98e73f3d5849c55d50
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-10-10 01:53:02 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,257 @@
+// "დამოუკიდებელი სამუშაო 1"
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout <<"გაიღიმე,გაიღიმე, გაიღიმე" << endl;
+//     cout <<"გაიღიმე,გაიღიმე " << endl;
+//     cout <<"გაიღიმე" << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 2"
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a = 3;
+//     double b = a / 2;
+//     cout << "a გაიყო ორზე და გამოიყო მისი მთელი ნაწილი"<< b<< endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(1)"
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a = (3+7)*12;
+//     cout << "a უდრის-" << a << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(2)"
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int c =15;
+//     double b = c/5+ 8.35;
+//     cout << "c უდრის-"<< b<<endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(3)"
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int b = (14%3 +2)/5;
+//     cout<< "b უდრის-" << b << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(4)"
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a =2;
+//     int b = 1;
+//     int c = 4;;
+//     cout <<"გამოსახულებას გამოეყო მთელი რიცხვი და უდრის "<< (a+b+c) / 3<<"-ს"<< endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(5)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n(3);
+//     int m = {4};
+//     double d = (m + n)/2.0;
+//     cout << "გამოსახულება უდრის - " << d<< endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 3(6)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 9;
+//     int y = 14 -x/4;
+//     cout << " გამოსახულება უდრის " << y << "-ს" << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 4
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 10;
+//     double a = 0.5;
+//     int b = 3;
+//     int c = 100;
+//     int z = a*(x * x)+(b * x) + c;
+//     cout << z <<endl;
+//     return 0;
+// }
+
+
+// "დამოუკიდებელი სამუშაო 5
+// -6750; pi; 0.04; 921.054; -0.000075;
+// 10 *x;y +48; 3.25*a - 14*b; 42*(p*p)-8; c*c*c;
+// (b-a)/3; (x+y)*(x+y); (c*c)+(c*d)+(d*d); -5*(a*(x*x))+4.25*b*x-3*c;
+// k/m*n + mn/k;(a-b)/y1 + y2/(a+b); ((x1*x1) + (y1*y1))*(((x2*x2)- (y2*y2)));
+// (3*(p-q)+5*(p+q))*z; 2.7*(x+((x-2y)*(x-2y)+5.09y)); ((a+1)*(a+1)) / ((a*a)-1);
+
+
+
+// "დამოუკიდებელი სამუშაო 7
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float angle1, angle2, angle3;
+//     angle1 = 20;
+//     angle2 = 100;
+//     angle3 = 180-(angle1 + angle2);
+//     cout << "მესამე კუთხე ტოლია " << angle3 <<endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 8
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int rad,height;
+//     float cylVolume;
+//     rad = 5;
+//     height = 20;
+//     cylVolume = (3.14* rad *rad *height);
+//     cout << "ცილინდრის მოცულობა არის" << cylVolume << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 9
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int rad;
+//     float sphereVolume, pi  ;
+//     rad = 10;
+//     pi = 3.14;
+//     sphereVolume = (4 * pi * rad * rad * rad )/3;
+//     cout << "სფეროს მოცულობა არის " << sphereVolume << endl;
+//     return 0;
+
+
+
+// }
+// "დამოუკიდებელი სამუშაო 10(1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,h;
+//     float s;
+//     a = 7;
+//     h = 3;
+//     s= ( a * h )/2.;
+//     cout << "სამკუთხედის ფართობი არის " << s << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 10(2)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float l,pi,r;
+//     r = 10;
+//     pi = 3.14;
+//     l = 2 * pi *r;
+//     cout << "წრეწირის პერიმეტრი არის " << l << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 10(3)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float S,x,P;
+//     x = 2.5;
+//     S = x * x;
+//     P = x * 4;
+//     cout <<"კვადრატის ფართობი არის " << S << " და კვადრატის პერიმეტრი არის " << P << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 10(4)
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     float b,P,S;
+//     int a;
+//     a = 10;
+//     b = 2.5;
+//     P = 2 * ( a + b );
+//     S = a * b;
+//     cout <<"მართკუთხედის ფართობი არის " << S << " და მართკუთხედის პერიმეტრი არის " << P << endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 10(5)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float S,pi,R;
+//     R = 5.2;
+//     pi = 3.14;
+//     S = pi * R *R;
+//     cout << "წრეწირის ფართობი არის " << S <<endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 10(6)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c,x,y;
+//     float z;
+//     a = 12;
+//     b = 3;
+//     c = 4;
+//     x = a + b + c;
+//     y = a * b * c;
+//     z = x/3.;
+//     cout << "ჯამი არის " << x << ",ნამრავლი არის " << y << "და საშუალო არითმეტიკული არის" << z <<endl;
+//     return 0;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო 11 
+// char;int;bool;string;double; char;
+// bool;int;double;string; 
